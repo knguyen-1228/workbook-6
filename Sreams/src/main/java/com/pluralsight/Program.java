8d164689127098cc34e1cbf1a4031b44d32992e3
Kevin Nguyen <knguyen@appdev.yearup.org>
2025-05-21 15:40:38 -0500

    Exercise 2 streams

---
+++ Sreams/src/main/java/com/pluralsight/Program.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<Person>();
+
+        people.add(new Person("Kevin","Nguyen",25));
+        people.add(new Person("Alice","Johnson", 34));
+        people.add(new Person("Bob","Smith",28));
+        people.add(new Person("Carol","Williams",42));
+        people.add(new Person("David","Brown",19));
+        people.add(new Person("Eve","Jones", 50));
+        people.add(new Person("Frank","Miller",14));
+        people.add(new Person("Grace","Davis",65));
+        people.add(new Person("Hank","Wilson",79));
+        people.add(new Person("Jack","Taylor",40));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a first or last name to search: ");
+        String searchName = scanner.nextLine();
+
+        List<Person> matchingName = people.stream()
+                .filter(person -> person.getFirstName().equalsIgnoreCase(searchName) || person.getLastName().equalsIgnoreCase(searchName))
+                .toList();
+
+        if(matchingName.isEmpty()){
+            System.out.println("Nobody was found under the name: " + searchName);
+        }else {
+            System.out.println("People under the name: " + searchName);
+            matchingName.forEach(person -> System.out.println(matchingName));
+        }
+
+        double totalAge = people.stream()
+                .mapToInt(person -> person.getAge())
+                .sum();
+        System.out.println("Average age: " + (totalAge / people.size()));
+
+        int oldestAge = people.stream()
+                .mapToInt(person -> person.getAge())
+                .max()
+                .orElse(-1);
+        System.out.println("Oldest age: " + oldestAge);
+
+        int youngestAge = people.stream()
+                .mapToInt(person -> person.getAge())
+                .min()
+                .orElse(-1);
+        System.out.println("Youngest age: " + youngestAge);
+
+
+    }
+}
